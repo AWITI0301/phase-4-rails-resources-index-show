3d96ca80a1ed825699c4c2c630376dfd4bd13f7c
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-25 19:33:02 +0300

    commit tree a539b5a3cd0560f68680c92c11cea6a091180fe9

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,19 +1,17 @@
-lass BirdsController < ApplicationController
-
+class BirdsController < ApplicationController
   # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
-  end
+def index
+  birds = Bird.all
+  render json: birds
+end
 
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+# GET /birds/:id
+def show
+  bird = Bird.find_by(id: params[:id])
+  if bird
+    render json: bird
+  else
+    render json: { error: "Bird not found" }, status: :not_found
   end
-
+end
 end
